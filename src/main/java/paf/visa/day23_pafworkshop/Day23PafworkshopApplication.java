eb117d6acee63a0b4e432dc984bbdbdeecb73a38
jylmarcus <maplecow@gmail.com>
2023-06-22 12:10:05 +0800

    Initial commit

---
+++ src/main/java/paf/visa/day23_pafworkshop/Day23PafworkshopApplication.java
@@ -0,0 +1,13 @@
+package paf.visa.day23_pafworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day23PafworkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day23PafworkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/paf/visa/day23_pafworkshop/Day23PafworkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package paf.visa.day23_pafworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day23PafworkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
